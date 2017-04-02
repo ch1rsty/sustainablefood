2510e9af7ce7a48b0ae720d32a195e9df1aa7f0b
Chirsty McFadyen <cm271@st-andrews.ac.uk>
2017-04-02 19:35:48 +0100

    Add files via upload

--- sustainablefood.py
+++ sustainablefood.py
@@ -10,6 +10,22 @@ app = Flask("SustainableFood")
 def home():
     return render_template('index.html')
 
+jannettas = 0
+
+@app.route("/search", methods=['POST'])
+def search():
+    form_data = request.form
+    print form_data['location']
+    print form_data['dietary']
+    print form_data['cuisine']
+    print form_data['price']
+
+
+    
+    if form_data['location'] in open('jannettas.txt').read():
+                                 return true
+
+    return false
+
 if __name__ == '__main__':
     app.run()
-
